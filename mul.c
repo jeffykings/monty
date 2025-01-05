65abc412a8cbedd56742f47f192d130f6bbcf402
jeffykings <jeffykings@gmail.com>
2025-01-05 20:12:00 +0100

    mul_fix3

--- mul.c
+++ mul.c
@@ -19,7 +19,7 @@ void mul(stack_t **stack, unsigned int line_number)
 	}
 	else
 	{
-		fprintf(stderr, "L%d: can't mul stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 }
